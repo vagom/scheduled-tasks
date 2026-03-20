5718d798d581a74bb5000e19d1e9a4a88755a97d
vagom <vagom15@gmail.com>
2026-03-20 14:27:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -16,23 +16,31 @@ import os
 MY_EMAIL = os.environ.get("MY_EMAIL")
 MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+# Pick current date, month and day, in a tuple (month, day)
+today = (datetime.now().month, datetime.now().day)
 
+# Read the CVS file
 data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+# Convert it in a dictionary
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+# Check if the current date is on dictionary
+if today in birthdays_dict:
+    birthdays_person = birthdays_dict[today]
+    letter_path = f"letter_templates/letter_{randint(1, 3)}.txt"
+# Reads a random letter
+    with open(letter_path, 'r') as file:
+        letter_to_send = file.read()
+    letter_to_send = letter_to_send.replace("[NAME]", birthdays_person["name"])
+
+
+email_to_send = birthdays_person["email"]
+
+# Send the e-mail
+with smtplib.SMTP('smtp.gmail.com', 587) as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+    connection.sendmail(from_addr=my_email,
+                        to_addrs=email_to_send,
+                        msg=f"Subject:Happy Birthday!\n\n{letter_to_send}"
+                        )
