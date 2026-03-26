a556dbf6637bce9502775386790d9b8ffc8c350d
vagom <vagom15@gmail.com>
2026-03-26 19:18:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,58 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
-import random
-import smtplib
 import os
+import requests
+from twilio.rest import Client
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+# ******************** My e-mail credentials *********************
+my_email = os.environ.get("MY_EMAIL")
+my_password = os.environ.get("MY_PASSWORD")
+# ****************************************************************
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+# ******************** My mobile credentials *********************
+my_mobile = os.environ.get("MY_MOBILE")
+# ****************************************************************
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+# ******************** Piracicaba lat, lon ***********************
+MY_LAT = -22.725165
+MY_LON = -47.6493269
+# ****************************************************************
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+# ******************** OpenWeather parameters ********************
+owm_api_key = os.environ.get("OWM_API_KEY")
+owm_endpoint = os.environ.get("OWM_ENDPOINT")
+# ****************************************************************
+
+# ******************** Twilio parameters *************************
+account_sid = os.environ.get("TWILIO_SID")
+auth_token = os.environ.get("TWILIO_TOKEN")
+msg_serv_sid = os.environ.get("TWILIO_MSG_SID")
+# ****************************************************************
+
+parameters = {
+    "lat" : MY_LAT,
+    "lon" : MY_LON,
+    "cnt" : 4,
+    "APPID" : owm_api_key
+}
+
+response = requests.get(url=OWM_ENDPOINT, params=parameters)
+response.raise_for_status()
+weather_data = response.json()
+response_code = weather_data["cod"]
+
+will_rain = False
+
+for hour_data in weather_data["list"]:
+    condition_code = hour_data["weather"][0]["id"]
+    if condition_code <= 700:
+        will_rain = True
+
+if will_rain:
+    print(will_rain)
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        messaging_service_sid=msg_serv_sid,
+        body='Better bring an Umbrella! ☔',
+        from_='+12603193480',
+        to=my_mobile
+    )
+
