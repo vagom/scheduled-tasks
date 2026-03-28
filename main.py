838ba801a2220eadeb7f9058a23edec3f2d79ff1
Valdir Gomes <vagom15@gmail.com>
2026-03-28 14:48:15 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-import os
+ILimport os
 import requests
 from twilio.rest import Client
 
-# ******************** My e-mail credentials *********************
-my_email = os.environ.get("MY_EMAIL")
-my_password = os.environ.get("MY_PASSWORD")
-# ****************************************************************
+# # ******************** My e-mail credentials *********************
+# my_email = os.environ.get("MY_EMAIL")
+# my_password = os.environ.get("MY_EMAIL_PASSWORD")
+# # ****************************************************************
 
-# ******************** My mobile credentials *********************
+# ******************** Phone credentials *********************
 my_mobile = os.environ.get("MY_MOBILE")
+
 # ****************************************************************
 
 # ******************** Piracicaba lat, lon ***********************
@@ -25,16 +26,17 @@ owm_endpoint = os.environ.get("OWM_ENDPOINT")
 account_sid = os.environ.get("TWILIO_SID")
 auth_token = os.environ.get("TWILIO_TOKEN")
 msg_serv_sid = os.environ.get("TWILIO_MSG_SID")
+twilio_phone = os.environ.get("TWILIO_PHONE")
 # ****************************************************************
 
-parameters = {
+owm_params = {
     "lat" : MY_LAT,
     "lon" : MY_LON,
     "cnt" : 4,
     "APPID" : owm_api_key
 }
 
-response = requests.get(url=OWM_ENDPOINT, params=parameters)
+response = requests.get(url=owm_endpoint, params=owm_params)
 response.raise_for_status()
 weather_data = response.json()
 response_code = weather_data["cod"]
@@ -52,7 +54,7 @@ if will_rain:
     message = client.messages.create(
         messaging_service_sid=msg_serv_sid,
         body='Better bring an Umbrella! ☔',
-        from_='+12603193480',
+        from_=twilio_phone,
         to=my_mobile
     )
 
